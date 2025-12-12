5a0355e3d416d2bfbc5693e6b85297d662f3b026
Elias Hanelt <elias.hanelt@gmail.com>
2025-12-11 21:56:37 -0800

    remove code duplication

--- command/stonks.go
+++ command/stonks.go
@@ -1,99 +1,14 @@
 package command
 
 import (
-	"bytes"
-	"log"
-	"sort"
-	"time"
 	"waldbot/data"
-	"waldbot/date"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/wcharczuk/go-chart/v2"
 )
 
 func stonksResponse(query Query) (string, *discordgo.File) {
 	memberId := data.ShortUserId(query.member.User.ID)
-
-	dates := make([]date.Date, 0, len(data.DayData))
-
-	for date := range data.DayData {
-		dates = append(dates, date)
-	}
-
-	sort.Slice(dates, func(i, j int) bool {
-		return date.IsSmaller(dates[i], dates[j])
-	})
-
-	starting := true
-
-	var xValues []time.Time
-	var yValues []float64
-	var allTimeMinutes uint32
-	for _, date := range dates {
-		if !query.dateCondition(date) {
-			continue
-		}
-		var minutes uint32
-		for _, channel := range data.DayData[date].Channels {
-			for _, session := range channel {
-				if session.UserID == memberId {
-					minutes += uint32(session.Minutes)
-				}
-			}
-		}
-		if minutes > 0 || !starting {
-			starting = false
-			allTimeMinutes += minutes
-			xValues = append(xValues, time.Date(int(date.Year), time.Month(date.Month), int(date.Day), 0, 0, 0, 0, time.Local))
-			yValues = append(yValues, float64(allTimeMinutes))
-		}
-	}
-	if len(xValues) < 2 {
-		return "Nicht genug Daten gefunden!", nil
-	}
-	graph := chart.Chart{
-		Width:  1280,
-		Height: 720,
-		Background: chart.Style{
-			Padding: chart.Box{
-				Top:    80,
-				Left:   10,
-				Right:  10,
-				Bottom: 10,
-			},
-		},
-		ColorPalette: waldColorPalette,
-		Title:        "Stonks von " + data.EffectiveName(query.member),
-		XAxis: chart.XAxis{
-			Name:           "Datum",
-			ValueFormatter: chart.TimeDateValueFormatter,
-		},
-		YAxis: chart.YAxis{
-			Name: "Stunden",
-			ValueFormatter: func(v interface{}) string {
-				if typed, isTyped := v.(float64); isTyped {
-					return data.FormatTime(uint32(typed))
-				}
-				return "error"
-			},
-		},
-		Series: []chart.Series{
-			chart.TimeSeries{
-				XValues: xValues,
-				YValues: yValues,
-			},
-		},
-	}
-	buffer := bytes.NewBuffer([]byte{})
-	err := graph.Render(chart.PNG, buffer)
-	if err != nil {
-		log.Println("Error while creating diagram: ", err)
-        return "Diagram error", nil
-	}
-    return "", &discordgo.File {
-        Name: "stonks.png",
-        ContentType: "image/png",
-        Reader: bytes.NewReader(buffer.Bytes()),
-    }
+	dates := getSortedDates()
+	xValues, yValues := buildCumulativeData(dates, query.dateCondition, &memberId)
+	return renderChart(xValues, yValues, "Stonks von "+data.EffectiveName(query.member), "Stunden")
 }
--- command/stonkslope.go
+++ command/stonkslope.go
@@ -1,15 +1,9 @@
 package command
 
 import (
-	"bytes"
-	"log"
-	"sort"
-	"time"
 	"waldbot/data"
-	"waldbot/date"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/wcharczuk/go-chart/v2"
 )
 
 func stonkslopeResponse(query Query) (string, *discordgo.File) {
@@ -17,95 +11,7 @@ func stonkslopeResponse(query Query) (string, *discordgo.File) {
 		return "Daywindow muss zwischen 1 und 100 sein", nil
 	}
 	memberId := data.ShortUserId(query.member.User.ID)
-
-	dates := make([]date.Date, 0, len(data.DayData))
-
-	for date := range data.DayData {
-		dates = append(dates, date)
-	}
-
-	sort.Slice(dates, func(i, j int) bool {
-		return date.IsSmaller(dates[i], dates[j])
-	})
-
-	starting := true
-
-	var xValues []time.Time
-	var yValues []float64
-	var nday uint = 0
-	for j, date := range dates {
-		nday++
-
-		var minutes uint32
-		var dayscounted uint32
-		for i := 0; i < int(query.daywindow); i++ {
-			if j+i >= len(dates) {
-				break
-			}
-			if !query.dateCondition(dates[j+i]) {
-				continue
-			}
-			dayscounted++
-			for _, channel := range data.DayData[dates[j+i]].Channels {
-				for _, session := range channel {
-					if session.UserID == memberId {
-						minutes += uint32(session.Minutes)
-					}
-				}
-			}
-		}
-
-		if minutes > 0 || !starting {
-			starting = false
-			xValues = append(xValues, time.Date(int(date.Year), time.Month(date.Month), int(date.Day), 0, 0, 0, 0, time.Local))
-			yValues = append(yValues, float64(minutes)/float64(dayscounted))
-		}
-	}
-	if len(xValues) < 2 {
-		return "Nicht genug Daten gefunden!", nil
-	}
-	graph := chart.Chart{
-		Width:  1280,
-		Height: 720,
-		Background: chart.Style{
-			Padding: chart.Box{
-				Top:    80,
-				Left:   10,
-				Right:  10,
-				Bottom: 10,
-			},
-		},
-		ColorPalette: waldColorPalette,
-		Title:        "Stonkslope von " + data.EffectiveName(query.member),
-		XAxis: chart.XAxis{
-			Name:           "Datum",
-			ValueFormatter: chart.TimeDateValueFormatter,
-		},
-		YAxis: chart.YAxis{
-			Name: "Stunden/Tag",
-			ValueFormatter: func(v interface{}) string {
-				if typed, isTyped := v.(float64); isTyped {
-					return data.FormatTime(uint32(typed))
-				}
-				return "error"
-			},
-		},
-		Series: []chart.Series{
-			chart.TimeSeries{
-				XValues: xValues,
-				YValues: yValues,
-			},
-		},
-	}
-	buffer := bytes.NewBuffer([]byte{})
-	err := graph.Render(chart.PNG, buffer)
-	if err != nil {
-		log.Println("Error while creating diagram: ", err)
-		return "Diagram error", nil
-	}
-	return "", &discordgo.File{
-		Name:        "stonks.png",
-		ContentType: "image/png",
-		Reader:      bytes.NewReader(buffer.Bytes()),
-	}
+	dates := getSortedDates()
+	xValues, yValues := buildRollingAverageData(dates, query.dateCondition, &memberId, query.daywindow)
+	return renderChart(xValues, yValues, "Stonkslope von "+data.EffectiveName(query.member), "Stunden/Tag")
 }
